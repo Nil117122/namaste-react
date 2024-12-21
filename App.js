671f9b29cefc13700d38103f78cde23bc885207f
Nilanjan Roy <nnil94144@gmail.com>
2024-12-21 18:30:33 +0530

    modified files

--- App.js
+++ App.js
@@ -1,31 +1,13 @@
-/**
- * 
- * 
- *  <div id="parent">
- *      <div id="child">
- *          <h1>I'm an h1 tag </h1>
- *          <h2>I'm an h2 tag </h2>
- *      </div>
- *       <div id="child2">
- *          <h1>I'm an h1 tag </h1>
- *          <h2>I'm an h2 tag </h2>
- *      </div>
- *  </div> 
- */
-const parent=React.createElement(
-    "div"
-    ,{id:"parent"},
-    [ React.createElement(
-        "div",{id:"child"},[ 
-            React.createElement("h1",{},"I'm an h1 tag"),
-            React.createElement("h2",{},"I'm an h2 tag")
-        ]),
-        React.createElement(
-            "div",{id:"child2"},[ 
-                React.createElement("h1",{},"I'm an h1 tag"),
-                React.createElement("h2",{},"I'm an h2 tag")
-            ])]
-    );
-const heading=React.createElement("h1",{id:"heading"},"Hello World from React!!");
-const root=ReactDOM.createRoot(document.getElementById("heading"));
-root.render(parent);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            
+        </div>
+    )
+}
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render();
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 #Namaste React
+
+#Parcel
+-Dev builds
+-Local Server
+-HMR - Hot Module Replacement
+-File watching algorithm - written in c++
+-Caching - for faster builds
+-Image optimization
+-Minification
+-Bundling
+-compress
+-Consistent hashing
+-Code spliting
+-Differential bundling - support older browsers
+-Diagonostic
+-Error handling
+-Tree Shaking
+-Different dev and prod builds
+
+
