26a0141692b1ebfed13c46fe16aef2bd05400a51
Nilanjan Roy <nnil94144@gmail.com>
2025-01-13 23:45:13 +0530

    useEffect and fetch api

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,66 +1,70 @@
 import RestrurentCard from "./RestrurentCard";
-import resObj from "../utils/mockData";
-import { useState } from "react";
+import Shimmer from "./Shimmer";
+import { useState, useEffect } from "react";
 const Body = () => {
-  const [listOfRestrurent,setListOfRestrurent] = useState(resObj);
-  const listOfRestrurentJS = [
-    {
-      info: {
-        id: "10576",
-        name: "Pizza Hut",
-        cloudinaryImageId:
-          "RX_THUMBNAIL/IMAGES/VENDOR/2024/7/16/87e56c63-b521-4257-96ae-a42229b92009_10576.jpg",
-        costForTwo: "₹350 for two",
-        cuisines: ["Pizzas"],
-        avgRating: 3.2,
-        sla: {
-          slaString: "20-25 mins",
-        },
-      }
-    },
-    {
-      info: {
-        id: "10577",
-        name: "KFC",
-        cloudinaryImageId:
-          "RX_THUMBNAIL/IMAGES/VENDOR/2024/7/16/87e56c63-b521-4257-96ae-a42229b92009_10576.jpg",
-        costForTwo: "₹350 for two",
-        cuisines: ["Pizzas"],
-        avgRating: 4.2,
-        sla: {
-          slaString: "20-25 mins",
-        },
-      }
-    },
-    {
-      info: {
-        id: "10578",
-        name: "Dominos",
-        cloudinaryImageId:
-          "RX_THUMBNAIL/IMAGES/VENDOR/2024/7/16/87e56c63-b521-4257-96ae-a42229b92009_10576.jpg",
-        costForTwo: "₹350 for two",
-        cuisines: ["Pizzas"],
-        avgRating: 4.2,
-        sla: {
-          slaString: "20-25 mins",
-        },
-      }
-    }
-  ];
-    return (
-      <div className="body">
-        <div className="filter">
-          <button className="filter-btn" onClick={()=>{
-            const filteredList = listOfRestrurent.filter((res)=>res.info.avgRating>=4.5)
-            setListOfRestrurent(filteredList)
-            }}>
-            Top Rated Restrurent
-          </button>  
-        </div>
-        <div className="res-container">
-          {listOfRestrurent.map((restrurent) => <RestrurentCard key={restrurent.info.id} resData={restrurent}/>)}
-        </div>
-      </div>
+  const [listOfRestrurent, setListOfRestrurent] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [filteredRestrurent, setFilteredRestrurent] = useState([]);
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
+  const fetchApi = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
+    const res = await data.json();
+   
+    
+    setListOfRestrurent(
+      res.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+    );
+    setFilteredRestrurent(res.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+    
+  };
+
+
+  return listOfRestrurent.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="filter">
+        <div className="search">
+          <input
+           type="text"
+           className="search-in" 
+           value={searchText}
+           onChange={(e) =>{
+            setSearchText(e.target.value);
+          }}
+           />
+          <button 
+          onClick={()=>{
+            const filteredRestrurent = listOfRestrurent.filter((res)=>res.info.name.toLowerCase().includes(searchText.toLowerCase()));
+            console.log(filteredRestrurent);
+            setFilteredRestrurent(filteredRestrurent);
+          }}
+          >Search</button>
+        </div>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestrurent.filter(
+              (res) => res.info.avgRating >= 4.5
+            );
+            setListOfRestrurent(filteredList);
+          }}
+        >
+          Top Rated Restrurent
+        </button>
+      </div>
+      <div className="res-container">
+        {filteredRestrurent.map((restrurent) => (
+          <RestrurentCard key={restrurent.info.id} resData={restrurent} />
+        ))}
+      </div>
+    </div>
+  );
 };
 export default Body;
